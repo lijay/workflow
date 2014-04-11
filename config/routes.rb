d5bf18400dbb6d0235744f5944fd0abec922168f
lijay chen <lijay.chen@gmail.com>
2014-04-11 13:21:03 +0800

    update index ui

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Workflow::Application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  root :to => 'high_voltage/pages#show', :id => 'welcome'
+  #root :to => 'high_voltage/pages#show', :id => 'welcome'
+  root :to => 'projects#index'
 
   resources :projects do
   	resources :steps
